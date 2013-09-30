991f77291efa9a1683e25b0bcba3b05833d2611a
mjbright <github@mjbright.net>
2013-09-30 13:45:58 -0700

    Cleaned up script, added comments

--- novaclient_list.py
+++ novaclient_list.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
 import novaclient
-import novaclient.auth_plugin
 from novaclient import client
-from novaclient import exceptions as exc
-import novaclient.extension
-from novaclient.openstack.common import strutils
-from novaclient import utils
-from novaclient.v1_1 import shell as shell_v1_1
-from novaclient.v3 import shell as shell_v3
+#import novaclient.auth_plugin
+#from novaclient import exceptions as exc
+#import novaclient.extension
+#from novaclient.openstack.common import strutils
+#from novaclient import utils
+#from novaclient.v1_1 import shell as shell_v1_1
+#from novaclient.v3 import shell as shell_v3
 
 import os
 
-# Get authentication variables from environment (source devstack/openrc):
+##################################################
+# Get authentication variables from environment:
+# Note: In DevStack environment
+#           source devstack/openrc
+
 user = os.getenv('OS_USERNAME')
 if user == None:
     die("OS_USERNAME not set - source your openrc")
@@ -30,13 +34,17 @@ if os_auth_url == None:
     die("OS_AUTH_URL not set")
 
 api='1.1'
+api='2' # Will use API 1.1
 
+##################################################
+# Connect to nova:
 print "Connecting using " + "USER="+user + ", PWORD="+pword + ", URL="+os_auth_url
-
 nt = client.Client(api, user, pword, tenant, os_auth_url)
-
-print
 print "Connected"
+
+##################################################
+# List information about resources:
+print
 print "FLAVORS=" + str(nt.flavors.list())
 print "INSTANCES(servers)=" + str(nt.servers.list())
 print "IMAGES=" + str(nt.images.list())
