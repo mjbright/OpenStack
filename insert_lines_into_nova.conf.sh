245dec0aac6edfd88185fbe3cc93ae4035e99939
mjbright <github@mjbright.net>
2013-10-03 13:35:37 -0700

    Removed quota_instances modifications

--- insert_lines_into_nova.conf.sh
+++ insert_lines_into_nova.conf.sh
@@ -3,7 +3,7 @@ NOVA_CONF=/etc/nova/nova.conf
 NOVA_CONF_ORIG=/etc/nova/nova.conf.orig
 
 QUOTA_CORES_LINE=quota_cores=-1
-QUOTA_INSTANCES_LINE=quota_instances=20
+#QUOTA_INSTANCES_LINE=quota_instances=10
 
 ##############################################################################
 # Functions:
@@ -44,12 +44,12 @@ $QUOTA_CORES_LINE
 }
 #&& echo "quota_cores line already present";
 
-grep quota_instances $NOVA_CONF || {
-    echo "No quota_instances line in $NOVA_CONF, adding $QUOTA_INSTANCES_LINE";
-    sudo sed "/\[DEFAULT\]/a\
-$QUOTA_INSTANCES_LINE
-" -i.bak2 $NOVA_CONF;
-}
+#grep quota_instances $NOVA_CONF || {
+#    echo "No quota_instances line in $NOVA_CONF, adding $QUOTA_INSTANCES_LINE";
+#    sudo sed "/\[DEFAULT\]/a\
+#$QUOTA_INSTANCES_LINE
+#" -i.bak2 $NOVA_CONF;
+#}
 #&& echo "quota_instances line already present";
 
 ls -altr $NOVA_CONF $NOVA_CONF_ORIG
