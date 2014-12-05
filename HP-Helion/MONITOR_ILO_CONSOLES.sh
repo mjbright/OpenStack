f5006a1123c9b8b3d1470a69eae9440d9aab29cf
Mike Bright <github@mjbright.net>
2014-12-05 15:28:44 +0100

    Updated MONITOR_ILO_CONSOLES.sh to read ilo information from baremetal.csv

--- HP-Helion/MONITOR_ILO_CONSOLES.sh
+++ HP-Helion/MONITOR_ILO_CONSOLES.sh
@@ -1,24 +1,68 @@
 
-SESSION_NAME="ILO_GEN9"
-ILOS="10.3.247.116 10.3.247.117 10.3.247.118 10.3.247.119 10.3.247.120 10.3.247.121 10.3.247.122 10.3.247.123"
+BAREMETAL_CSV=baremetal.csv
+SESSION_NAME="ILO_UNKNOWN"
+TMUX=tmux
+#TMUX="echo XXX"
+ILO_USER="UNKNOWN"
+
+#ILOS="10.3.247.116 10.3.247.117 10.3.247.118 10.3.247.119 10.3.247.120 10.3.247.121 10.3.247.122 10.3.247.123"
 #ILOS="ig9_1 ig9_2 ig9_3 ig9_4 ig9_5 ig9_6 ig9_7 ig9_8"
 
-ILO_USER=nfv
+die() {
+    echo "$0: die - $*" >&2
+    exit 1
+}
+
+# Read ilo nodes from baremetal.csv
+getILONodes() {
+    CSVFILE=$1; shift
+
+    ILOS=""
+    for line in `sed 's/ *, */,/g' $CSVFILE`;do
+        IFS=',' read -a array <<< $line
+        mac=${array[0]}
+        user=${array[1]}
+        pass=${array[2]}
+        NODE=${array[3]}
+        cpu=${array[4]}
+        ram=${array[5]}
+        disk=${array[6]}
+        ILOS+="$NODE "
+    done
+}
+
+while [ ! -z "$1" ];do
+    case $1 in
+        -csv) shift; BAREMETAL_CSV=$1;;
+        -s)   shift; SESSION_NAME=$1;;
+
+        *)    die "Unknown option '$1'";;
+    esac
+    shift
+done
+
+[ ! -f $BAREMETAL_CSV ] && die "No baremetal.csv file $BAREMETAL_CSV"
+
+getILONodes $BAREMETAL_CSV
+
+echo "ILOS=$ILOS"
+#exit 0
+
 
 SSH_OPTS="-o MACS=hmac-sha1,hmac-md5 -i /home/mjb/.ssh/ILO_dsa -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
 
 echo "Old sessions:"
-tmux ls
+$TMUX ls
 
 echo "Killing $SESSION_NAME sessions:"
-tmux kill-session -t $SESSION_NAME
+$TMUX kill-session -t $SESSION_NAME
 
 echo "Old sessions:"
-tmux ls
+$TMUX ls
 
 echo
 echo "Creating new-session $SESSION_NAME"
-tmux new-session -s $SESSION_NAME -d 'bash'
+$TMUX new-session -s $SESSION_NAME -d 'bash'
 
 DATETIME=$(date +%Y-%m-%d-%Hh%Mm%S)
 DIR=/home/mjb/ILO/${DATETIME}_${SESSION_NAME}.logs
@@ -27,18 +71,16 @@ DIR=/home/mjb/ILO/${DATETIME}_${SESSION_NAME}.logs
 
 echo
 for ILO in $ILOS;do
-    #tmux new-window -t $SESSION_NAME -d 'echo $ILO |& cat; bash'
     WINDOW_NAME=${ILO##*.}
     WINDOW_NAME=${WINDOW_NAME##*_}
     echo $WINDOW_NAME
-    #tmux new-window -t ${SESSION_NAME}:${WINDOW_NAME} -d 'echo $ILO |& cat; bash'
-    tmux new-window -t ${SESSION_NAME}:${WINDOW_NAME} -d "while true;do ssh $SSH_OPTS ${ILO_USER}@${ILO} textcons |& stdbuf -oL tee -a $DIR/ILO.log.${ILO}.raw; done"
+
+    ILO_USER=$(grep $ILO $BAREMETAL_CSV | awk -F, '{ print $2; }')
+
+    $TMUX new-window -t ${SESSION_NAME}:${WINDOW_NAME} -d "while true;do ssh $SSH_OPTS ${ILO_USER}@${ILO} textcons |& stdbuf -oL tee -a $DIR/ILO.log.${ILO}.raw; done"
 done
 
-#tmux new-window -t ILO_GEN9 -d 'bash'
-#tmux new-window -t ILO_GEN9 -d 'bash'
-#tmux new-window -t ILO_GEN9 -d 'bash'
 echo
 echo "New sessions:"
-tmux ls
+$TMUX ls
 
