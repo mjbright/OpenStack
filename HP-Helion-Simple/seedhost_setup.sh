ee0c110b8cd8778c52dc0f6d74f0bcdff3093948
Mike Bright <github@mjbright.net>
2015-01-27 17:05:39 +0100

    Added commands to setup INSTALL directory

--- HP-Helion-Simple/seedhost_setup.sh
+++ HP-Helion-Simple/seedhost_setup.sh
@@ -1,10 +1,23 @@
 
+INSTALL_DIR=/root/INSTALL
+GIT=/root/src/git/Openstack/HP-Helion-Simple
+TOOLS_GIT=${GIT%-Simple}
+
+########################################
+# functions:
 die() {
     echo "$0 - die: $*" >&2
     exit 1
 }
 
 [ `id -un` != 'root' ] && die "Must be run as root"
+[     $GIT != `pwd`  ] && die "Expected to be run from dir <$GIT>"
+
+########################################
+# main:
+
+echo
+echo; echo "Creating ssh key if needed, installing needed packages, restarting libvirtd ..."; echo
 
 # Create root rsa key
 [ ! -f /root/.ssh/id_rsa ] && ssh-keygen -t rsa -N ""
@@ -22,5 +35,18 @@ apt-get install -y $PACKAGES
 
 # Restart libvirt-bin:
 /etc/init.d/libvirt-bin restart
+
+########################################
+# install these tools:
+
+echo
+echo; echo "Installing links to tools in install dir <$INSTALL_DIR>"; echo
+
+[ ! -d $INSTALL_DIR ] && mkdir -p $INSTALL_DIR
+
+cd $INSTALL_DIR
+
+ln -s $GIT/*.sh .
+ln -s $TOOLS_GIT/TOOLS.sh .
 
 
