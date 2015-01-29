16a5a379336aef3866a27e605b8cc5207dbd64e0
Mike Bright <github@mjbright.net>
2015-01-29 15:47:57 +0100

    Added die() function, added checks on JSON file and environment variables

--- HP-Helion-Simple/init.sh
+++ HP-Helion-Simple/init.sh
@@ -2,19 +2,28 @@
 
 set -o nounset # Force error on unset variables
 
+die() {
+    echo "die: $0 - $*" >&2
+    exit 1
+}
+
 [ -f ./env_vars ] && {
     echo ". ./env_vars"
     . ./env_vars
 }
 
-[ -f $JSON ] && {
-    echo "source $LOAD_CONFIG_SH $JSON"
-    set +o nounset # Force error on unset variables
-    source $LOAD_CONFIG_SH $JSON
-    set -o nounset # Force error on unset variables
-}
+[ ! -f $JSON ] && die "No such json file <$JSON>"
+
+echo "source $LOAD_CONFIG_SH $JSON"
+set +o nounset # Force error on unset variables
+source $LOAD_CONFIG_SH $JSON
+set -o nounset # Force error on unset variables
 
 env | grep -q BM_NETWORK_SEED_IP || die "Missing variable definitions"
+env | grep 192.0 && {
+    env | grep 192.0
+    die "Looks like there are undefined variable (192.0.*)"
+}
 
 cd /root;
 
