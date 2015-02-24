22054b5384e6ec4047893d18ec59a9cc718481b8
Mike Bright <github@mjbright.net>
2015-02-24 17:29:27 +0100

    Corrected some early bugs in backup script

--- HP-Helion-Simple/helion_seed_uc_backup.sh
+++ HP-Helion-Simple/helion_seed_uc_backup.sh
@@ -1,7 +1,9 @@
 
 DATETIME=$(date +%Y-%m-%d_%Hh)
+START=$(date +%S)
+
 BACKUP_DIR=/root/BACKUP.RESTORE/backups/${DATETIME}
-LOGFILE=${BACKUPDIR}/${DATETIME}.log
+LOGFILE=${BACKUP_DIR}/${DATETIME}.log
 
 SCRIPTS=/root/tripleo/tripleo-incubator/scripts/
 
@@ -9,14 +11,16 @@ SEED_QCOW=/var/lib/libvirt/images/seed.qcow2
 
 [ ! -d $BACKUP_DIR ] && mkdir -p $BACKUP_DIR
 
-[ -z "$1" ] && exec $0 -logging |& tee $LOGFILE
+[ -z "$1" ] && { exec $0 -logging |& tee $LOGFILE; exit; }
 
 # Assuming no race condition on exec/LOGFILE/date:
 echo "Logging to $LOGFILE"
 
 die() {
     echo "$0: die - $*" >&2
-    exit
+    echo "Output Logged to $LOGFILE"
+    END=$(date +%S); echo "Took $((END-START)) secs"
+    exit 1
 }
 
 SEED_QCOW_SIZE=$(wc -c < $SEED_QCOW)
@@ -28,18 +32,15 @@ DF_KBYTES_AVAIL_MB=$(( DF_KBYTES_AVAIL / 1024 ))
 NINETYPC_AVAIL=$(( 900 * $DF_KBYTES_AVAIL ))
 NINETYPC_AVAIL_MB=$(( NINETYPC_AVAIL / 1024 / 1024 ))
 
-#echo "SEED_QCOW_SIZE=$SEED_QCOW_SIZE"
-#echo "DF_KBYTES_AVAIL=$DF_KBYTES_AVAIL"
+echo
 echo "SEED_QCOW_SIZE[MBy]=$SEED_QCOW_SIZE_MB"
 echo "DF_KBYTES_AVAIL[MBy]=$DF_KBYTES_AVAIL_MB"
 echo "NINETYPC_AVAIL[MBy]=$NINETYPC_AVAIL_MB"
-
-#echo "if [ $NINETYPC_AVAIL_MB -le $SEED_QCOW_SIZE_MB ];then"
-
-echo ; echo "Sufficient disk space for seed.qcow backup - proceeding ..."
+echo
 if [ $NINETYPC_AVAIL_MB -le $SEED_QCOW_SIZE_MB ];then
     die "Insufficient space for seed VM backup (${NINETYPC_AVAIL_MB}MBy < seedVM ${SEED_QCOW_SIZE_MB}MBy)"
 fi
+echo ; echo "Sufficient disk space for seed.qcow backup - proceeding ..."
 
 $SCRIPTS/hp_ced_backup.sh --seed -f $BACKUP_DIR
 
@@ -51,11 +52,15 @@ NINETYPC_AVAIL_MB=$(( NINETYPC_AVAIL / 1024 / 1024 ))
 
 EXTIMATED_UNDERCLOUD_SIZE_MB="30000"
 
-echo ; echo "Sufficient disk space for seed.qcow backup - proceeding ..."
+echo
+echo "EXTIMATED_UNDERCLOUD_SIZE_MB[MBy]=$EXTIMATED_UNDERCLOUD_SIZE_MB"
+echo "DF_KBYTES_AVAIL[MBy]=$DF_KBYTES_AVAIL_MB"
+echo "NINETYPC_AVAIL[MBy]=$NINETYPC_AVAIL_MB"
+echo
 if [ $NINETYPC_AVAIL_MB -le $EXTIMATED_UNDERCLOUD_SIZE_MB ];then
     die "Insufficient space for seed VM backup (${NINETYPC_AVAIL_MB}MBy < estimated UC backup size ${EXTIMATED_UNDERCLOUD_SIZE_MB}MBy)"
 fi
-die "BAD"
+echo ; echo "Sufficient disk space for underloud backup - proceeding ..."
 
 $SCRIPTS/hp_ced_backup.sh --undercloud -f $BACKUP_DIR
 
@@ -64,5 +69,6 @@ df
 echo "Output Logged to $LOGFILE"
 ls -altr $LOGFILE
 
+END=$(date +%S); echo "Took $((END-START)) secs"
 exit 0
 
